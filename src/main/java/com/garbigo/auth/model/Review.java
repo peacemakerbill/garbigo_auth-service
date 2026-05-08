64f0b458ae57d3d002b2e713697389e339d2ee2a
Bill Graham Peacemaker <billgrahamp@gmail.com>
2026-05-08 22:18:18 +0300

    refractoring like, follow and review functionality

--- src/main/java/com/garbigo/auth/model/Review.java
+++ src/main/java/com/garbigo/auth/model/Review.java
@@ -2,23 +2,22 @@ package com.garbigo.auth.model;
 
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
 @Data
 @Document(collection = "reviews")
+@CompoundIndex(name = "review_idx", def = "{'userId': 1, 'targetId': 1, 'targetType': 1}")
 public class Review {
     private String id;
     private String userId;
     private String targetId;
+    private String targetType;
     private int rating;
     private String comment;
 
     @CreatedDate
     private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
 }
