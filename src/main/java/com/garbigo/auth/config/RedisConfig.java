df3b24889b9ed4463f406689878e2e52e303122c
Bill Graham Peacemaker <billgrahamp@gmail.com>
2026-05-13 22:12:51 +0300

    Updated Redis Configuration file

--- src/main/java/com/garbigo/auth/config/RedisConfig.java
+++ src/main/java/com/garbigo/auth/config/RedisConfig.java
@@ -1,22 +1,28 @@
 package com.garbigo.auth.config;
 
-import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedissonClient redissonClient() {
-        Config config = new Config();
-        config.useSingleServer()
-              .setAddress("redis://localhost:6379")
-              .setConnectionPoolSize(10)
-              .setConnectionMinimumIdleSize(5)
-              .setTimeout(5000);
-        return Redisson.create(config);
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // Use JSON serializer for objects
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
+
+        return template;
     }
 }
